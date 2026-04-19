2a86d47222a69105f7cb49af39a3719e8b897033
MACantara <cantara.michaelangelo@gmail.com>
2026-04-19 19:26:56 +0800

    Fix path to bootstrap file in error handling

--- public/errors/error.php
+++ public/errors/error.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../app/bootstrap.php';
+require_once __DIR__ . '/../../app/bootstrap.php';
 
 $code = (int) ($_GET['code'] ?? 500);
 $title = (string) ($_GET['title'] ?? 'Server Error');
